a9d5891b5ce419b891838614aaad141eac5187da
Daniel Beltran <genserdanie-123@hotmail.com>
2022-11-24 20:58:31 +0100

    documents instalations who is springboot_tematics

---
+++ msvc-usuarios/src/test/java/org/aguzman/springcloud/msvc/usuario/MsvcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aguzman.springcloud.msvc.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
