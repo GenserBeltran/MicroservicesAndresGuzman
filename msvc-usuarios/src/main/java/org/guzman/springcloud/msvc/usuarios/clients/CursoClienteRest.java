8b9da3d98ce0b78d9b2ffc74a2bb91b26a3dca7c
Daniel Beltran <genserdanie-123@hotmail.com>
2022-12-13 11:22:58 +0100

    class_op Feign entre microservicios IntroducDocker

--- msvc-usuarios/src/main/java/org/guzman/springcloud/msvc/usuarios/clients/CursoClienteRest.java
+++ msvc-usuarios/src/main/java/org/guzman/springcloud/msvc/usuarios/clients/CursoClienteRest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "msvc-curso", url = "http://localhost:9000", path = "/api/curso")
+@FeignClient(name = "msvc-curso", url = "host.docker.internal:9000", path = "/api/curso")
 public interface CursoClienteRest {
 
     //Metodo para eliminar usuario LO QUE HACEMOS ES ELIMINAR LA RELACION desasignar el uysario del curso
