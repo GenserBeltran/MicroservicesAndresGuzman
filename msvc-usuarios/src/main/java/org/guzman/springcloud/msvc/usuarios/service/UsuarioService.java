4f3893044014e390012e2b40c103d54654155f85
Daniel Beltran <genserdanie-123@hotmail.com>
2022-12-20 17:13:35 +0100

    Class_op conceptos y practica

---
+++ msvc-usuarios/src/main/java/org/guzman/springcloud/msvc/usuarios/service/UsuarioService.java
@@ -0,0 +1,25 @@
+package org.guzman.springcloud.msvc.usuarios.service;
+
+import org.guzman.springcloud.msvc.usuarios.model.entity.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+
+    List<Usuario> listar();
+
+    Optional<Usuario> porId(Long id);
+
+    Usuario guardar(Usuario usuario);
+
+    void eliminar(Long id);
+
+    Optional<Usuario> porEmail(String email);
+
+    //Alternativa III
+    boolean byEmailExists(String email);
+
+    //listar todos los usuarios por id
+    List<Usuario> listarPorIds(Iterable<Long> ids);
+}
