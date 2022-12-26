4d2a052757daefa4ad5d0ce28f7501108c71077c
Daniel Beltran <genserdanie-123@hotmail.com>
2022-12-26 10:36:44 +0100

    Class_Op concep_practica super util docker_compose

---
+++ kubernetes/msvc-usuarios/src/main/java/org/aguzman/springcloud/msvc/usuarios/model/entity/Usuario.java
@@ -0,0 +1,31 @@
+package org.aguzman.springcloud.msvc.usuarios.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "El campo Nombre no puede estar vacio") //No puede llegar vacio
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @NotEmpty
+    @Email
+    @Column(name = "email", nullable = false, length = 100, unique = true)
+    private String email;
+
+    @NotEmpty
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+}
